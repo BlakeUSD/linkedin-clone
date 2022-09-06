4282b8b39c5e766aff455c0bf9ec6680e1a389bb
Dellano Blake <dellano.blake@outlook.com>
2022-09-06 17:59:47 -0500

    commit

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-const delayedColorChange = (color, delay) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            document.body.style.backgroundColor = color;
-            resolve();
-        }, delay);
-    });
-};
-
-async function rainbow() {
-    await delayedColorChange("red", 1000);
-    await delayedColorChange("orange", 1000);
-    await delayedColorChange("yellow", 1000);
-    await delayedColorChange("green", 1000);
-    await delayedColorChange("blue", 1000);
-    await delayedColorChange("indigo", 1000);
-    await delayedColorChange("violet", 1000);
-}
-
-rainbow();
-
 //Button Functionality Section
 let count = 0;
 const counterText = document.getElementById("counterText");
@@ -37,23 +16,4 @@ document.getElementById("decrease").addEventListener('click', decreaseCount);
 function decreaseCount() {
     count--;
     handleChange();
-}
-
-const filterFalsy = (value) => {
-    return !value;
-}
-const filterTruthy = (value) => {
-    return !!value;
-}
-
-function isOddNum(num) {
-    return num % 2 !== 0 ? true : false;
-}
-
-function isEven(num) {
-    return num % 2 === 0 ? true : false;
-}
-
-function isInteger(num) {
-    if (parseInt(num));
 }
